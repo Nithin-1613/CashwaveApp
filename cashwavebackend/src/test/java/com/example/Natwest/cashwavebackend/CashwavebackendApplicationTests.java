cebccd77d456aa2bf5f0e73fcff25e7eb3a785e5
nithinrhaj <nithinrhaj@gmail.com>
2023-09-26 14:30:19 +0530

    initial commit

---
+++ cashwavebackend/src/test/java/com/example/Natwest/cashwavebackend/CashwavebackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Natwest.cashwavebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashwavebackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
